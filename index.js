4af164c669047174188e2cdc9ef232bd0f239643
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-03-01 16:27:35 +0600

    others added

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const resultList = phones => {
         document.getElementById('error-message').style.display = 'block';
     }
     phones.forEach(phone => {
-        console.log(phone)
+        // console.log(phone)
         const phoneDetails = document.getElementById('phone-full-details');
         phoneDetails.textContent = '';
         const div = document.createElement('div');
@@ -59,27 +59,59 @@ const phoneDetailsfetch = phoneID => {
 
 
 const phonefulldetails = phone => {
-    console.log(phone);
     const phoneDetails = document.getElementById('phone-full-details');
-    const other = phone.others
+    const others = phone.others
+    const mainFeatures = phone.mainFeatures
+
+    // const sensors = JSON.stringify(phone.mainFeatures.sensors).split(',').join(', ');
+    const sensor = phone.mainFeatures.sensors;
+    const p1 = document.createElement('p');
+    // p1.classList.add('card-text')
+    p1.className = "card-text";
+
+
+    if (phone.others != undefined)
+        for (const [key, value] of Object.entries(phone.others)) {
+            const p = document.createTextNode(`${key} : ${value};`);
+            // var text = document.createTextNode("\n");
+            var br = document.createElement("br");
+            p1.appendChild(p)
+            p1.appendChild(br)
+                // console.log(p)
+                // console.log(p1)
+                // console.log(key)
+                // console.log(value)
+        }
+        // <p class="card-text ">Bluetooth: ${others.Bluetooth}</p>
+        // <p class="card-text ">GPS ${others.GPS}</p>
+        // <p class="card-text ">NFC: ${others.NFC}</p>
+        // <p class="card-text ">Radio: ${others.Radio}</p>
+        // <p class="card-text ">USB: ${others.USB}</p>
+        // <p class="card-text ">WLAN: ${others.WLAN}</p>
+        // <p class="card-text ">WLAN: ${others}</p>
     phoneDetails.textContent = '';
-    const div = document.createElement('div');
+    console.log(sensor);
     // div.classList.add('card');
+
+    const div = document.createElement('div');
     div.innerHTML = `
     <div class = "card mt-5 mb-4 bg-success bg-opacity-10">
         <img src="${phone.image}" class="card-img-top w-50 mx-auto mt-5" alt="...">
         <div class="card-body">
             <h5 class="card-title">Phone Name: ${phone.name}</h5>
             <p class="card-text mb-5">Brand Name: ${phone.brand}</p>
-            <p class="card-text ">Chip Set: ${phone.mainFeatures.chipSet}</p>
-            <p class="card-text ">Storage: ${phone.mainFeatures.storage}</p>
-            <p class="card-text ">Display Size: ${phone.mainFeatures.displaySize}</p>
-            <p class="card-text ">Memory: ${phone.mainFeatures.memory}</p>
-            <p class="card-text ">Bluetooth: ${other.Bluetooth}</p>
+            <p class="card-text ">Chip Set: ${mainFeatures.chipSet}</p>
+            <p class="card-text ">Storage: ${mainFeatures.storage}</p>
+            <p class="card-text ">Display Size: ${mainFeatures.displaySize}</p>
+            <p class="card-text ">Memory: ${mainFeatures.memory}</p>
+            <p class="card-text my-3">Sensors: ${sensor}</p>
+            
             <p class="card-text ">Release Date: ${phone.releaseDate}</p>
             
         </div>
     </div>
     `;
+    div.lastElementChild.lastElementChild.appendChild(p1)
+        // console.log('this is: ', div.lastChild)
     phoneDetails.appendChild(div);
 }
